6e6287d73b408563697d4e5455af5ba03daa864b
wwwanujchouhan <wwwanujchouhan@gmail.com>
2025-09-25 13:06:09 +0000

    Changes before Firebase Studio auto-run

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -107,14 +107,6 @@ const Hero = () => {
             View Events
           </button>
         </div>
-
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <div className="w-6 h-10 border-2 border-cyber-blue rounded-full flex justify-center">
-            <div className="w-1 h-3 bg-cyber-blue rounded-full mt-2 animate-pulse"></div>
-          </div>
-          <p className="text-cyber-blue text-sm font-tech mt-2">Scroll Down</p>
-        </div>
       </div>
 
       {/* Decorative Elements */}
